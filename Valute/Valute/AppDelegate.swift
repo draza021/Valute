ac81e02da0a3f0400ff89cec9c42c634d99868ae
Dragoslav Ignjatovic <draza021@gmail.com>
2018-05-03 00:30:50 +0200

    complete starting initial view controllers from code

--- Valute/Valute/AppDelegate.swift
+++ Valute/Valute/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //let vc = storyboard.instantiateViewController(withIdentifier: "ConvertController")
+        
+        let nc = storyboard.instantiateInitialViewController()
+        
+        //window?.rootViewController = vc
+        window?.rootViewController = nc
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Valute/Valute/ConvertController.swift
@@ -0,0 +1,14 @@
+//
+//  ConvertController.swift
+//  Valute
+//
+//  Created by Drago on 5/3/18.
+//  Copyright © 2018 nsiddevelopment. All rights reserved.
+//
+
+import UIKit
+
+final class ConvertController: UIViewController {
+
+
+}
