560075a8a6910c69146f53f9d13c40b4c38b9968
Dragoslav Ignjatovic <draza021@gmail.com>
2018-05-03 15:52:24 +0200

    completed upper layout

---
+++ Valute/Valute/CurrencyBoxView.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyBoxView.swift
+//  Valute
+//
+//  Created by Drago on 5/3/18.
+//  Copyright © 2018 nsiddevelopment. All rights reserved.
+//
+
+import UIKit
+
+final class CurrencyBoxView: UIView {
+
+    
+
+}
